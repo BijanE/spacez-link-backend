4c9eea77410fb11704f716917473466fcb5f3ac7
Bijan Etesam <56190823+BijanE@users.noreply.github.com>
2022-01-09 00:36:41 +0300

    Fix

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 // Impoert express
 const express = require("express");
+const cors = require("cors");
 
 // Import dotenv
 require("dotenv").config();
 
 const app = express();
+
+const corsOptions = {
+  //To allow requests from client
+  origin: [
+    "http://localhost:3000",
+    "http://127.0.0.1",
+    "https://www.spacez.link",
+    "https://spacez-link.herokuapp.com",
+  ],
+  credentials: true,
+  withCredentials: true,
+};
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
